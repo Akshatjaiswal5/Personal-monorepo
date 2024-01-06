b001db9af833d7805f13c34a52df99ba05c76b15
Akshatjaiswal5 <akshatjaiswal521@gmail.com>
2024-01-07 01:18:59 +0530

    added my-svelte-project

---
+++ web-server/my-svelte-project/src/App.svelte
@@ -0,0 +1,18 @@
+<script>
+  let count = 1;
+  let text = "you are 1 year old";
+  const incrementCount = () => {
+    count = count * 2;
+  };
+
+  $: {
+    text = "you are now " + count + " years old.";
+  }
+</script>
+
+<main>
+  <button on:click={incrementCount}>{text}</button>
+</main>
+
+<style>
+</style>
